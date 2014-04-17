05b6f7cff3bc67452a1325135616ec07c11f85bc
Yasser Al-Khder <yasser.alkhder@gmail.com>
2014-04-17 18:42:34 -0400

    Tweets now save to json file. Removed keys.

--- getTweets.rb
+++ getTweets.rb
@@ -1,18 +1,14 @@
 require 'twitter'
+require 'json'
+require_relative 'credentials'
 
-config = {
-  :consumer_key         =>  "2obJdnCcsfskkdDkgIzwke9Eq",
-  :consumer_secret      =>  "pnlVumuzUgjnwXr3AuEvREWaE8HMjgrVV1NV73E1V6stVapTdK",
-  :access_token         =>  "2447737650-Eb7axidllSjzY34yUMTnGRWjE8924QJ2p6gzrrL",
-  :access_token_secret  =>  "rswb7umdaZCqhYyyG0ipic5XLCCbTXVSX7oiKnwjYM6Cg"
-}
-
-client = Twitter::REST::Client.new(config)
-
+client = Twitter::REST::Client.new(@config)
 searchResult = client.search("a", {:lang  =>  'en'})
-
-# searchResult.each do |tweet|
-#     puts tweet.attrs
-# end
-
-puts searchResult.first.attrs
+# might get dupe tweets
+File.open("test.json", "a+") do |f|
+  searchResult.each do |tweet|
+    if tweet.hashtags?
+      f.write(tweet.attrs.to_json)
+    end
+  end
+end
