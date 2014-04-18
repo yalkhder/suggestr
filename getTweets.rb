d04b471ac6b5ff43edb69da3aaed7b377ce1a03b
Yasser Al-Khder <yasser.alkhder@gmail.com>
2014-04-17 21:02:09 -0400

    Fixed issue where concatenating json file was improper.

--- getTweets.rb
+++ getTweets.rb
@@ -3,12 +3,24 @@ require 'json'
 require_relative 'credentials'
 
 client = Twitter::REST::Client.new(@config)
-searchResult = client.search("a", {:lang  =>  'en'})
-# might get dupe tweets
-File.open("test.json", "a+") do |f|
+jsonFile = File.open("test.json", "a+")
+testArray = []
+10.times do
+  searchResult = client.search("a", {:lang  =>  'en'})
   searchResult.each do |tweet|
-    if tweet.hashtags?
-      f.write(tweet.attrs.to_json)
-    end
+    testArray << tweet.attrs
   end
 end
+
+# might not be the best way to load JSON from file and add new results to exisiting results
+previousArray = []
+if (s = jsonFile.gets)
+  previousArray = JSON.parse(s)
+end
+finalArray = testArray + previousArray
+
+puts testArray.count
+puts previousArray.count
+puts finalArray.count
+
+jsonFile.write(finalArray.to_json)
