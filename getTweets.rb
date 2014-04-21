75d7872e64bc719568b2e34306b8be830b25ee33
Stephen Kraemer <straemer@gmail.com>
2014-04-21 12:59:46 -0400

    pick random words to search for instead of always using 'a'

--- getTweets.rb
+++ getTweets.rb
@@ -4,9 +4,31 @@ require_relative 'credentials'
 
 testArray = []
 
+commonWords = [ "the", "be","to", "of", "and", "a", "in", "that", "have",
+                "I", "it", "for", "not", "on", "with", "he", "as", "you",
+                "do", "at", "this", "but", "his", "by", "from", "they",
+                "we", "say", "her", "she", "or", "an", "will", "my",
+                "one", "all", "would", "there", "their", "what", "so",
+                "up", "out", "if", "about", "who", "get", "which", "go",
+                "me", "when", "make", "can", "like", "time", "no", "just",
+                "him", "know", "take", "people", "into", "year", "your",
+                "good", "some", "could", "them", "see", "other", "than",
+                "then", "now", "look", "only", "come", "its", "over",
+                "think", "also", "back", "after", "use", "two", "how",
+                "our", "work", "first", "well", "way", "even", "new",
+                "want", "because", "any", "these", "give", "day", "most",
+                "us"
+              ];
+
 client = Twitter::REST::Client.new(@config)
 10.times do
-  searchResult = client.search("a", {:lang  =>  'en'})
+  searchTerm = commonWords.sample
+  9.times do
+    searchTerm += " OR " + commonWords.sample
+  end
+  puts searchTerm
+  searchResult = client.search(commonWords.sample,
+                               {:lang  =>  'en'})
   searchResult.each do |tweet|
     testArray << tweet.attrs
   end
